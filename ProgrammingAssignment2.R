e179058147087ce5139a17200eb05e0ec56942db
huuhoang2211 <hoanghuu2211@gmail.com>
2018-04-12 01:08:12 +0700

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,3 +1,5 @@
+## This function, makeCacheMatrix creates an object called "matrix" that can cache it's inverse:
+
 makeCacheMatrix <- function(m = matrix()) {
     inv <- NULL
     set <- function(x) {
@@ -10,7 +12,10 @@ makeCacheMatrix <- function(m = matrix()) {
     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
+## The following function calculates the inverse of the object "matrix" created with the above function. 
+## It will check if the inverse of the matrix has already been calculated or not.
+## If the inverse has already been calculated, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse function.
 cacheSolve <- function(m, ...) {
     inv <- m$getinverse()
     if(!is.null(inv)) {
